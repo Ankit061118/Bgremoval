f245a5568babfe55fb6c67c3c98fd2adff02258a
Ankit061118 <rawatankii3@gmail.com>
2026-08-19 17:57:13 +0530

    First controller

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import "dotenv/config"
 import cors from "cors"
 import connectDB from './config/mongoDb.js'
+import userRouter from './routes/userRoutes.js'
 
 const app=express()
 const PORT=process.env.PORT || 4000
@@ -14,6 +15,7 @@ app.use(cors())
 app.get('/',(req,res)=>{
     res.send("API working")
 })
+app.use("/api/user",userRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
